5a788ecd683c5807f170875fe12168e5442e38e9
nayong2021 <nayong2017@ajou.ac.kr>
2022-12-14 23:53:14 +0900

    Update baekjoon solution: #12852, #9019

---
+++ baekjoon_12852.py
@@ -0,0 +1,34 @@
+#https://www.acmicpc.net/problem/12852
+
+from collections import deque
+
+N = int(input())
+
+move = [0] * (N+1)
+
+queue = deque([N])
+
+while True:
+    cur = queue.popleft()
+    if cur == 1:
+        break
+    if cur % 3 == 0 and move[int(cur / 3)] == 0:
+        move[int(cur / 3)] = cur
+        queue.append(int(cur / 3))
+    if cur % 2 == 0 and move[int(cur / 2)] == 0:
+        move[int(cur / 2)] = cur
+        queue.append(int(cur / 2))
+    if move[cur - 1] == 0:
+        move[cur - 1] = cur
+        queue.append(cur - 1)
+
+
+road = [1]
+cur = 1
+while cur != N:
+    road.append(move[cur])
+    cur = move[cur]
+
+print(len(road) - 1)
+for i in road[::-1]:
+    print(i, end=' ')
---
+++ baekjoon_9019.py
@@ -0,0 +1,47 @@
+#https://www.acmicpc.net/problem/9019
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+def get_ds(n):
+    d1 = int(n / 1000)
+    n -= d1 * 1000
+    d2 = int(n / 100)
+    n -= d2 * 100
+    d3 = int(n / 10)
+    n -= d3 * 10
+    d4 = n
+    return d1, d2, d3, d4
+
+def D(n):
+    return 2 * n % 10000, 'D'
+
+def S(n):
+    return (n + 9999) % 10000, 'S'
+
+def L(n):
+    d1, d2, d3, d4 = get_ds(n)
+    return 1000 * d2 + 100 * d3 + 10 * d4 + d1, 'L'
+
+def R(n):
+    d1, d2, d3, d4 = get_ds(n)
+    return 1000 * d4 + 100 * d1 + 10 * d2 + d3, 'R'
+
+for _ in range(T):
+    A, B = map(int, input().split())
+    commands = [0] * 10000
+    commands[A] = ''
+    queue = deque([A])
+    while True:
+        current = queue.popleft()
+        if current == B:
+            break
+        for i, command in (D(current), S(current), L(current), R(current)):
+            if commands[i] == 0:
+                commands[i] = commands[current] + command
+                queue.append(i)
+    print(commands[B])
+    
