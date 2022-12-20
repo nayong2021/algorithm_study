e708ff43cc4c604bfdbc26690fb37f604fb12e72
nayong2021 <nayong2017@ajou.ac.kr>
2022-12-20 18:17:27 +0900

    Update baekjoon solution: #1197, #4195

---
+++ baekjoon_1197.py
@@ -0,0 +1,39 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+def find(x):
+    if parent[x] < 0:
+        return x
+    else:
+        ancestor = find(parent[x])
+        parent[x] = ancestor
+        return ancestor
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x==y:
+        return True, parent[x]
+    if parent[x] > parent[y]:
+        x, y = y, x
+    parent[x] += parent[y]
+    parent[y] = x
+    return False, parent[x]
+
+V, E = map(int, input().split())
+weight_sum = 0
+parent = [-1] * (V+1)
+edges = []
+for _ in range(E):
+    A, B, C = map(int, input().split())
+    edges.append((C, A, B))
+edges.sort()
+for C, A, B in edges:
+    is_union, child_num = union(A, B)
+    if is_union:
+        continue
+    weight_sum += C
+    if -child_num == V:
+        break
+print(weight_sum)
---
+++ baekjoon_4195.py
@@ -0,0 +1,33 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+def find(x):
+    if type(parent[x]) is not str and parent[x] < 0:
+        return x
+    else:
+        ancestor = find(parent[x])
+        parent[x] = ancestor
+        return ancestor
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x==y:
+        return parent[x]
+    else:
+        if parent[x] < parent[y]:
+            x, y = y, x
+        parent[x] += parent[y]
+        parent[y] = x
+        return parent[x]
+
+T = int(input())
+
+for _ in range(T):
+    F = int(input())
+    parent = {}
+    for i in range(F):
+        x, y = input().split()
+        parent[x] = parent.get(x, -1)
+        parent[y] = parent.get(y, -1)
+        print(-union(x,y))
